7ed5ca723978820b78dc9a828e0493a73934a63f
nolimitstech <johnkingsley4eva@yahoo.com>
2023-01-16 13:36:16 +0100

    recent

--- Chibuike Jonas JavaScript/09-Data-Structures-Operators/starter/script.js
+++ Chibuike Jonas JavaScript/09-Data-Structures-Operators/starter/script.js
@@ -7,7 +7,7 @@ const flights =
 
   */
 ///////// Destructing of objects
-
+/*
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -225,7 +225,7 @@ console.log(i, j, k);
 */
 
 ///////////////SHORT CIRCUTING "OR LOGICAL OPERATOR"
-
+/*
 console.log(3 || "jonas"); //// 3 will be reportd to console cos the first value is a truthy value
 
 console.log(undefined || 0 || '' || 'Hello' || 23 || null); /////returns "Hello"cos its the first truthy value
@@ -249,3 +249,128 @@ if (restaurant.orderPizza) {
 ///the code can be rewritten using AND shortcircuting thus;
 restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
 
+/////Nullish coalescent Operator
+////Nullish values are Null and Undefined (Not 0 or '')
+restaurant.numGuests = 0;
+const guest3 = restaurant.numGuests ?? 10;
+console.log(guest3); //// result is 0, cos 0 is a nullish value nt null or undefined in this case
+
+*/
+
+/////////CODING CHALLENGE
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this 
+challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are
+ field players. For Bayern Munich (team 1) create one variable ('gk') with the
+  goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 
+  field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+ new array ('players1Final') containing all the original team1 players plus 'Thiago',
+  'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1',
+ 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names
+ (NOT an array) and prints each of them to the console, along with the number of 
+ goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which
+ team is more likely to win, WITHOUT using an if/else statement or the ternary
+  operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  Club1: 'Bayern Munich',
+  Club2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//////////SOLUTION TO CODING CHALLENGE
+///1. destructing the game player array
+const [player1, player2] = game.players;
+console.log(player1, player2);
+
+//2. Using the rest pattern
+const [gk, ...fieldplayers] = player1;
+console.log(gk, fieldplayers);
+
+const [gk2, ...fieldplayers2] = player2;
+console.log(gk2, fieldplayers2);
+
+///3. using the spread operator
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+////4. to add the 3 substitutes to player1 final list
+const players1Final = [...player1, 'Thiago', 'Coutinho', 'Perusic'];
+console.log(players1Final);
+
+////5. to create one variable for each odd
+
+///dnt knw why code aint working
+/*
+const {
+  odd: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+*/
+
+////6. to print the number of goals scored to the console
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+
+}
+
+printGoals('lewandowski', 'Muller', 'ken', 'james');
+
+//// 7. to print the team more likely to win based on the odd variable
+/// Logical operator is the best fit
+//// n/b code nt working, Team1 is undefined
+team1 > team2 && console.log('Team1 more likely to win');
+team1 < team2 && console.log('Team2 more likely to win');
